9ef55490a7ce364f1567e809a906ca477f718e5b
WyattRichardson <wyattcrichardson@gmail.com>
2023-03-04 16:53:56 -0500

    Added mouse dragging for camera movement.

--- src/Camera/Camera.gd
+++ src/Camera/Camera.gd
@@ -1,4 +1,12 @@
 extends Node2D
+#-----------------Vars---------------
+var lastMousePos = Vector2();
+var isMouseDragging = false;
+
+
+
+
+#-----------------Functions---------------
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,6 +15,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#Key camera movement.
 	var cameraVelocity = Vector2()
 	var speed = 1000
 	cameraVelocity = Vector2()
@@ -16,5 +25,18 @@ func _process(delta):
 		speed *= -1
 		cameraVelocity = Vector2(speed, 0)
 	self.translate(cameraVelocity * delta)
+	
+	
+
+func _input(event):
+	#Mouse camera movement.
+	if event.is_action_pressed("move_camera_with_mouse"):
+		isMouseDragging = true
+	elif event.is_action_released("move_camera_with_mouse"):
+		isMouseDragging = false
+	if isMouseDragging:
+		var deltaCam = get_global_mouse_position() - lastMousePos
+		self.translate(Vector2(-deltaCam.x, 0))
+	lastMousePos = get_global_mouse_position()
 	
 	
