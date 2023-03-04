d5d0825c45b3e2b3a08839722c117a5ae352a96b
Greggster990 <greggalexkinch@gmail.com>
2023-03-03 22:28:34 -0500

    added camera and camera movement

---
+++ src/Camera/Camera.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_process(true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(str(position.x))
+	var cameraVelocity = Vector2()
+	var speed = 1000
+	cameraVelocity = Vector2()
+	if Input.is_action_pressed("move_camera_right")and position.x <= 2875:
+		cameraVelocity = Vector2(speed, 0)
+	if Input.is_action_pressed("move_camera_left") and position.x >= 585:
+		speed *= -1
+		cameraVelocity = Vector2(speed, 0)
+	self.translate(cameraVelocity * delta)
+	
+	
