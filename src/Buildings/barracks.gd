d5775e6f8baf4bcffcd7f121ff5e6922394f9cd6
WyattRichardson <wyattcrichardson@gmail.com>
2023-03-06 18:04:44 -0500

    Added basic soldier spawning from barracks.

--- src/Buildings/barracks.gd
+++ src/Buildings/barracks.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 
-@export var health := 500 
+@export var baseSpawnRate = 1
+@export var level := 1
+@export var health := 500
+var timer = 0; 
 const COST = 100
+
+const soldierScene = preload("res://src/Military/soldier_basic.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,14 +16,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	timer += delta
+	if timer >= (baseSpawnRate * level):
+		spawn_soldier()
+		timer = 0
 	
 	
 func take_hit(damage: int):
 	health -= damage
 	if health < 1:
 		destroy_barracks()
-
+		
+func spawn_soldier():
+	var soldier = soldierScene.instantiate()
+	soldier.set_global_position(global_position)
+	get_tree().get_root().add_child(soldier)
 
 func destroy_barracks() -> void:
 	queue_free()
+	
+func level_up():
+	level += 1
