bcded9ea7e24393a97e42a573e48638fa4d59b0f
WyattRichardson <wyattcrichardson@gmail.com>
2023-03-06 23:11:03 -0500

    bank gives you money

--- src/Buildings/bank.gd
+++ src/Buildings/bank.gd
@@ -1,8 +1,9 @@
 extends StaticBody2D
 @export var level := 1
 @export var health := 500
+@export var baseIncome := 10
 const COST = 100
-
+var timer = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,7 +11,10 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	timer += delta
+	if timer >= (10):
+		PlayerData.money += (baseIncome*level)
+		timer = 0
 	
 func take_hit(damage: int):
 	health -= damage
