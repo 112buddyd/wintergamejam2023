82eeaccfca096b63eac84bc963395f8be8513dfc
Greggster990 <greggalexkinch@gmail.com>
2023-03-06 21:33:20 -0500

    changes spawn point

--- src/Buildings/barracks.gd
+++ src/Buildings/barracks.gd
@@ -37,12 +37,12 @@ func take_hit(damage: int):
 func spawn_soldier():
 	var soldier = soldierScene.instantiate()
 	get_tree().get_root().add_child(soldier)
-	soldier.global_position.x = 0
+	soldier.global_position.x = 420
 	soldier.global_position.y = randf_range(158, 470)
 func spawn_tank():
 	var tank = tankScene.instantiate()
 	get_tree().get_root().add_child(tank)
-	tank.global_position.x = 0
+	tank.global_position.x = 420
 	tank.global_position.y = randf_range(158, 470)
 	
 func destroy_barracks() -> void:
