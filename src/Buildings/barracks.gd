b65b4f3dde65732521d143a70ad7346b45879002
Greggster990 <greggalexkinch@gmail.com>
2023-03-06 22:22:34 -0500

    added hp bars

--- src/Buildings/barracks.gd
+++ src/Buildings/barracks.gd
@@ -19,6 +19,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	get_node("ProgressBar").set_value(health/500)
 	timer += delta
 	if timer >= (baseSpawnRate / level):
 		if level >= 2:
