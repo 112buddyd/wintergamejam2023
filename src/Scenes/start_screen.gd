79a6f4107d410205bc6e121a348a6a4a1261f562
Greggster990 <greggalexkinch@gmail.com>
2023-03-08 21:06:56 -0500

    mean menu buttons

--- src/Scenes/start_screen.gd
+++ src/Scenes/start_screen.gd
@@ -3,3 +3,11 @@ extends Control
 
 func _on_play_button_button_up():
 	get_tree().change_scene_to_file("res://src/Scenes/BattleScene.tscn")
+
+
+func _on_help_button_button_up():
+	$ColorRect2.visible = true
+
+
+func _on_hide_button_button_up():
+	$ColorRect2.visible = false
